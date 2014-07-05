b77016dfc088b68a3623a1cf802c1b71b01178da
shanyscarlet <shanygabizon@gmail.com>
2014-07-05 16:53:01 +0300

    2nd commit

--- Test/src/com/example/test/Test.java
+++ Test/src/com/example/test/Test.java
@@ -16,7 +16,8 @@ public class Test extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.test, menu);
-		return true;
+		
+		return false;
 	}
 
 }
